13ea5b5cc9f85b68a80ed9b29e5c3369d1b4106b
Gyenge Elemér <gyenge@valahol>
2026-04-13 13:20:37 +0200

    Kezdés

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class MainController {
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    void initialize() {}
+
+    @FXML
+    void onClickSaveButton(ActionEvent event) {
+
+    }
+
+}
